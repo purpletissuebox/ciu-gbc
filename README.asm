6b370ada2185cf40cb9a02eaed29b30a6f76890b
purple tissue box <dirksenmatthew1@gmail.com>
2023-11-14 16:43:29 -0500

    Improved directory traversal method for build script.

--- README.asm
+++ README.asm
@@ -2,16 +2,15 @@
 ;
 ;BUILD INSTRUCTIONS
 ;shortcut for windows users:
-;1. open the included build/build.bat.
-;2. replace the builddir variable with the path to the build/ folder on your machine.
-;3. run build.bat. the rom will be made in build/ciu.gbc.
-;4. debug files ciu.sym and ciuMap.txt can be removed for casual play.
+;1. run the included build/build.bat. the rom will be made in build/ciu.gbc.
+;2. debug files ciu.sym and ciuMap.txt can be removed for casual play.
 ;
 ;otherwise, to build it yourself:
 ;1. install rgbasm, rgblink, and rgbfix. tested using v0.5.1. (https://github.com/gbdev/rgbds/releases/tag/v0.5.1)
 ;2. navigate to <install directory>/build.
 ;3. assemble: rgbasm -p 0xFF -h -Weverything -o <object file> ../ciu.asm
 ;4. link: rgblink -p 0xFF -o <rom name> <object file>
+;4b. optional flags -m <map file> and -n <symbol file> can be used to build debug information along with the rom.
 ;5. fix checksums: rgbfix -p 0xFF -f hg <rom name>
 ;6. the object file can now be removed.
 ;
--- build/build.bat
+++ build/build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-SET builddir=C:\path\to\installation\build
+SET builddir=%0\..\
 
 SET options1=-p 0xFF -h -Weverything             -o ciu.o    ..\ciu.asm
 SET options2=-p 0xFF -m ciuMap.txt    -n ciu.sym -o ciu.gbc   .\ciu.o
@@ -9,13 +9,11 @@ SET options3=-p 0xFF -f hg                          ciu.gbc
 pushd .
 cd %builddir%
 
-echo %cd%
-
 .\rgbasm.exe  %options1%
 .\rgblink.exe %options2%
 .\rgbfix.exe  %options3%
 
-DEL ciu.o
+DEL .\ciu.o
 
 popd
 pause
