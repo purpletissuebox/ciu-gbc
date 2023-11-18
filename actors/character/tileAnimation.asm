a7586f19e2251c786ffebd159276fe22a50887e4
purple tissue box <dirksenmatthew1@gmail.com>
2023-11-18 16:11:10 -0500

    Explained bit setting math for looping down tiles vertically.

--- actors/character/tileAnimation.asm
+++ actors/character/tileAnimation.asm
@@ -1,5 +1,10 @@
 SECTION "CHARACTER TILESTREAM", ROMX
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;loads 4 tiles into vram repeatedly, slowly scrolling the pixels on each of those tiles right and up.
+;reads scroll registers and an internal timer to figure out how far to shift the tiles each frame such that they appear to move independently of the rest of the bkg layer.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 VARIABLE = $0003
 TIMER = $000E
 
@@ -7,110 +12,125 @@ tileAnimation:
 .init:
 	swapInRam shadow_scroll
 	ld a, $50
-	ld [shadow_scroll+1], a
+	ld [shadow_scroll+1], a ;start with left panel offscreen
 	restoreBank "ram"
 	ld de, tileAnimation.arrow_task
-	call loadGraphicsTask
+	call loadGraphicsTask ;we will modify the arrow tiles in-place, so the graphics task can be precomputed
 	updateActorMain tileAnimation.main
 
 .main:
-	ld hl, ACTORSIZE - 2
+	ld hl, ACTORSIZE - 1
 	add hl, bc
 	ldd a, [hl]
-	and a
+	or [hl] ;if other actors run before us, we might miss an update on the scroll registers and ruin the illusion! so make sure we are the final actor to run
 	jr z, .lastActor
 	
+		;we are going to spawn a "clone" - a new, byte-for-byte copy of ourself. simply spawning a replacement won't work, since the scroll timer is initialized to 0.
+		dec hl
 		ldh a, [next_actor+1]
 		ldd [hl], a
 		ldh a, [next_actor]
-		ld [hl], a
+		ld [hl], a ;find where the clone will load in
 		ld e, c
 		ld d, b
-		call spawnActor
+		call spawnActor ;load it in
 
 		ld hl, ACTORSIZE - 4
 		add hl, bc
-		ldi a, [hl]
+		ldi a, [hl] ;get clone's address back
 		ld h, [hl]
 		ld l, a
 		ld e, c
 		ld d, b
-		push bc
+		push de
 		ld c, LOW(ACTORSIZE - 2)
-		rst $10
+		rst $10 ;copy all of our memory into the clone (except the final pointer to maintain the linked list)
 		
 		pop de
 		jp removeActor
 		
 	.lastActor:
+	;first calculate which tiles to use assuming the camera is standing still
 	push bc
 	ld hl, VARIABLE
 	add hl, bc
-	ld a, [hl]
+	ld a, [hl] ;get scroll speed
 	ld hl, TIMER
 	add hl, bc
 	add [hl]
 	ldi [hl], a
 	ld a, $00
-	adc [hl]
-	and $0F
+	adc [hl] ;add to current position
+	and $0F ;wrap around every 16 pixels
 	ld [hl], a
+	ld b, a ;save nominal position for later
 	
-	ld b, a
+	;now we need to account for the camera. every 2 bytes of tile data represents one row of pixels, so we can scroll vertically by offsetting the start address by 2.
+	;however, to scroll horizontally we need to wrap bits across tiles, which is difficult. instead use a lookup table of 16 tiles, each offset by 1px.
+	
 	swapInRam shadow_scroll
 	ld a, [shadow_scroll+1]
 	add b
-	and $0F
+	and $0F ;calculate real x position = nominal position + camera
 	ld e, $00
 	rra
 	rr e
 	rra
 	rr e
-	ld d, a
+	ld d, a ;de = distance from start of tile lookup table. multiply by (4 tiles * 16 bytes/tile) = 64
 	
 	ld a, [shadow_scroll]
 	cpl
 	inc a
 	add b
-	and $0F
-	add a
-	ld c, a
+	and $0F ;calculate real y position. increase in y scroll actually moves the camera down, so invert the value before adding.
+	add a ;offset start address by 2 bytes per pixel
+	
 	ld hl, arrow_tiles
-	add hl, de
+	add hl, de ;go to correct tile
 	add l
-	ld l, a
+	ld l, a ;go to correct pixel within that tile
+	
+	;now we have to copy tiles into ram, wrapping correctly when we reach the bottom of the tile.
+	;tiles are saved in rom vertically and aligned to the nearest $40 to help with this step.
+	;copy 2 tiles at a time. the first two will end with XX00 and can be copied forwards. the next two end with XX20 and are copied backwards.
 	
 	ld de, animated_tiles
 	ld b, $10
 	.loopUp:
-		res 5, l
+		res 5, l ;after incrementing the src address, we might overflow from XX1F to XX20 so clear that bit to wrap
 		ldi a, [hl]
 		ld [de], a
 		inc de
 		ldi a, [hl]
 		ld [de], a
 		inc de
-		dec b
+		dec b ;copy 16 rows of pixels
 	jr nz, tileAnimation.loopUp
 	
 	dec hl
 	ld de, animated_tiles + $003F
 	ld b, $10
 	.loopDown:
-		set 5, l
+		set 5, l ;protect against underflow from XX20 to XX1F
 		ldd a, [hl]
 		ld [de], a
 		dec de
 		ldd a, [hl]
 		ld [de], a
 		dec de
-		dec b
+		dec b ;copy the other 16 rows of pixels
 	jr nz, tileAnimation.loopDown
 	
 	restoreBank "ram"
 	pop bc
 	call submitGraphicsTask
 	ret
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+.respawn:
+	NEWACTOR tileAnimation.init, $B0
 	
 .arrow_task:
 	GFXTASK animated_tiles, bkg_tiles0, $0000
