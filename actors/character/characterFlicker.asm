c2c5c4e88d22316afa694ea0cfcdd76eeecdf8d3
purple tissue box <dirksenmatthew1@gmail.com>
2023-11-19 11:35:05 -0500

    Improved descriptions.

--- actors/character/characterFlicker.asm
+++ actors/character/characterFlicker.asm
@@ -1,5 +1,11 @@
 SECTION "TEXT FLICKER", ROMX
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;controls sprite colors for character select text
+;flickers a palette between yellow and blue, then makes the blue color fade to white when a selection is made
+;reads input each frame to determine which color palette to change and what colors to put there
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 VARIABLE = $0003
 TIMER = $0004
 OBJOFFSET = $004E
@@ -7,87 +13,93 @@ OBJOFFSET = $004E
 characterFlicker:
 	ld hl, TIMER
 	add hl, bc
-	inc [hl]
+	inc [hl] ;increment timer
 	ldd a, [hl]
-	and $04
-	rrca
+	and $04 ;flash text on for 4 frames, off for 4 frames
+	rrca ;convert timer value into offset into color array
 	ld de, characterFlicker.colors
 	add e
 	ld e, a
 	ld a, d
 	adc $00
-	ld d, a
+	ld d, a ;de points to color we want displayed
 	
 	swapInRam shadow_palettes
 	
-	ld a, [hl]
+	ld a, [hl] ;get variable = which palette to write to
 	add a
 	add a
-	add a
+	add a ;palettes are 8 bytes in size
 	ld hl, shadow_palettes + OBJOFFSET
 	add l
 	ld l, a
 	ld a, h
 	adc $00
-	ld h, a
+	ld h, a ;hl points to destination palette
 	
 	
 	ld a, [de]
 	inc de
 	ldi [hl], a
 	ld a, [de]
-	ldi [hl], a
+	ldi [hl], a ;copy over desired color
 	
 	restoreBank "ram"
 	
 	ldh a, [press_input]
 	ld e, a
-	and $01
+	and $01 ;if A was pressed, change modes
 		jr nz, characterFlicker.submit
 	
 	ld a, e
 	and $30
-	ret z
+		ret z ;if L/R were not pressed, dont bother checking for anything
 	swap a
-	rra
+	rra ;rotate bottom bit (right) into carry flag
 	ld a, $01
 	jr c, characterFlicker.right
-		ld a, $00
+		ld a, $00 ;load a with 1 if right was pressed, 0 if left was pressed
 	.right:
+	
 	ld hl, VARIABLE
 	add hl, bc
 	cp [hl]
-	ret z
+		ret z ;if we are already flickering on that side, do nothing
 	
 	ld [hl], a
 	ld de, characterFlicker.revert
-	call spawnActor
+	call spawnActor ;put both color palettes back to yellow and we will begin flicking the correct side next frame
 	ret
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	
 .submit:
 	updateActorMain characterFlicker.shine
 	ld hl, TIMER
 	add hl, bc
-	ld [hl], $00
+	ld [hl], $00 ;some cleanup before we begin the next phase of color changes
+	
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 .shine:
 	ld hl, TIMER
 	add hl, bc
-	inc [hl]
+	inc [hl] ;increment timer
 	ldd a, [hl]
 	cp $40
-	jr z, characterFlicker.end
+		jr z, characterFlicker.end ;after 64 frames, the fade will be over so return
+	
 	add a
 	ld de, characterFlicker.colors
 	add e
 	ld e, a
 	ld a, d
 	adc $00
-	ld d, a
+	ld d, a ;de = pointer to upcoming color
 	
 	swapInRam shadow_palettes
 	
-	ld a, [hl]
+	ld a, [hl] ;get variable
 	add a
 	add a
 	add a
@@ -96,13 +108,13 @@ characterFlicker:
 	ld l, a
 	ld a, h
 	adc $00
-	ld h, a
+	ld h, a ;hl points to destination color palette
 		
 	ld a, [de]
 	inc de
 	ldi [hl], a
 	ld a, [de]
-	ldi [hl], a
+	ldi [hl], a ;copy it over
 	
 	restoreBank "ram"
 	ret
@@ -111,6 +123,8 @@ characterFlicker:
 		ld e, c
 		ld d, b
 		jp removeActor
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 		
 .revert:
 	NEWACTOR setColorsOBJ, $82
