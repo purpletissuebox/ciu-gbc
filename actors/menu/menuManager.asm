b5dc7db1e2ff911977665e7cd1edbe16873cccb4
purple tissue box <dirksenmatthew1@gmail.com>
2023-11-28 16:47:30 -0500

    Spawning of menu scene init actors now follows a staggered pattern.

--- actors/menu/menuManager.asm
+++ actors/menu/menuManager.asm
@@ -6,59 +6,40 @@ SECTION "MENU MANAGER", ROMX
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 TIMER = $000F
-ACTORLIST = $0004
-FIRSTVARIABLE = $0007
 
 menuManager:
 	ld hl, TIMER
 	add hl, bc
 	ld a, [hl]
 	inc [hl]
-	cp $60 ;wait for timer
-		ret nz
+	sub $60 ;wait for timer
+		ret c
 	
-	ld hl, ACTORLIST
-	add hl, bc
-	ld de, menuManager.actor_table
-	push bc
-	ld c, LOW(menuManager.end - menuManager.actor_table)
-	rst $10
-	pop bc ;copy actor list into local memory
-
-
+	cp ((menuManager.end - menuManager.actor_table) >> 2)
+	jr nz, menuManager.spawnNext
+		ld e, c
+		ld d, b
+		jp removeActor
+	
+	.spawnNext:	
+	ld l, a
 	swapInRam last_played_song
-		ld hl, last_played_song
-		ld e, [hl]  ;get last played song
+	ld a, [last_played_song]
+	ld h, a ;h = last played song, l = actor index to spawn
 	restoreBank "ram"
+		
+	ld de, menuManager.actor_table
+	ld a, l
+	add a
+	add a
+	add e
+	ld e, a
+	ld a, d
+	adc $00
+	ld d, a ;de = ptr to actor
 	
-	ld a, e
-	ld de, FIRSTVARIABLE - ACTORLIST + 1 ;de = distance between actors
-	ld hl, FIRSTVARIABLE
-	add hl, bc ;hl = ptr to first actor's variable
-	
-	.variableLoop:
-		bit 0, [hl] ;check for terminator
-			jr nz, menuManager.summon ;if found, begin spawning them in
-		ld [hl], a ;otherwise, save most recent song as that actor's variable
-		add hl, de
-		jr menuManager.variableLoop
-	.summon:
-	
-	ld hl, ACTORLIST
-	add hl, bc
-	ld e, l
-	ld d, h ;de = ptr to first actor
-	ld a, ((menuManager.end - menuManager.actor_table) >> 2) ;a = number of actors to spawn
-	.actorLoop:
-		ldh [scratch_byte], a ;a = loop index
-		call spawnActor
-		ldh a, [scratch_byte]
-		dec a
-	jr nz, menuManager.actorLoop
-
-	ld e, c
-	ld d, b
-	jp removeActor
+	ld a, h
+	jp spawnActorV
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
