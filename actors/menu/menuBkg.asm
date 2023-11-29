754e761da20e73f3299c7d963cb653a788a0bd25
purple tissue box <dirksenmatthew1@gmail.com>
2023-11-28 22:50:10 -0500

    Added description.

--- actors/menu/menuBkg.asm
+++ actors/menu/menuBkg.asm
@@ -1,7 +1,8 @@
 SECTION "MENU BKG WRAPPER", ROMX
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;
+;spawns child actors to load new background graphics on the menu scene.
+;is passed a song ID in, which it will calculate a scroll fraction to pass to its children.
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 VARIABLE = $0003
@@ -13,56 +14,56 @@ DELAYLONG = $0A
 
 menuBkg:
 	push bc
-	ld hl, VARIABLE
+	ld hl, VARIABLE ;read song ID
 	add hl, bc
 	ldi a, [hl]
 	cp $80 ;sets carry if we are scrolling up
 	jr c, menuBkg.up
 	
-		add $08
+		add $08 ;if scrolling down, the graphics are located 8 bands below the current song
 		and $3F
-		or $80
-		ldi [hl], a
-		ld [hl], $00
-		ld a, $01
+		or $80 ;flag as downward scroll
+		ldi [hl], a ;save variable
+		ld [hl], $00 ;no post-adjustment necessary
+		ld a, $01 ;pre-adjustment of 1
 		jr menuBkg.start
 		
 	.up:
-		dec a
+		dec a ;if scrolling up, the graphics are 1 band above the screen
 		and $3F
-		ldi [hl], a
-		ld [hl], $FF
-		xor a
+		ldi [hl], a ;save variable
+		ld [hl], $FF ;post-adjustment of -1
+		xor a ;no pre-adjustment necessary
 	
 	.start:
 	ld e, a
-	call menuBkg.adjust
+	call menuBkg.adjust ;do the pre-adjustment
 	pop bc
 	ld hl, CHILDVAR
 	add hl, bc
 	ld a, [hl]
 	ld de, menuBkg.high_priority_actors
-	call spawnActorV
+	call spawnActorV ;get variable back and spawn the scroll actor
 	
 	ld hl, TIMER
 	add hl, bc
-	ld [hl], DELAYSHORT
+	ld [hl], DELAYSHORT ;initalize timer
 	updateActorMain menuBkg.snooze
 	ret
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-.snooze:
+.snooze: ;waits DELAYSHORT frames before spawning the low priority actors
 	ld hl, TIMER
 	add hl, bc
 	dec [hl]
 		ret z
 	
-	ld [hl], DELAYLONG
+	ld [hl], DELAYLONG ;reload timer with longer delay
 	ld hl, CHILDVAR
 	add hl, bc
 	ld a, [hl]
-	ldh [scratch_byte], a
+	ldh [scratch_byte], a ;get variable and stash it for reuse
 	updateActorMain menuBkg.wait
 	ld c, ((menuBkg.high_priority_actors - menuBkg.low_priority_actors) >> 2)
 	
@@ -75,16 +76,16 @@ menuBkg:
 		ld e, a
 		ld a, d
 		adc $00
-		ld d, a
-		ldh a, [scratch_byte]
-		call spawnActorV
+		ld d, a ;de = ptr to next actor in list
+		ldh a, [scratch_byte] ;fetch variable
+		call spawnActorV ;spawn actor with correct variable
 		dec c
 	jr nz, menuBkg.spawnLoop
 	ret
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-.wait:
+.wait: ;wait for timer to expire and perform post-adjustment
 	ld hl, TIMER
 	add hl, bc
 	dec [hl]
