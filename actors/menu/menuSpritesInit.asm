2f1588208a89a5f71b4a67fdbb61ddbc3f349658
purple tissue box <dirksenmatthew1@gmail.com>
2023-11-30 22:12:40 -0500

    Greatly reduced actor's workload. Actor now fixes LCDC register and loads starting text.

--- actors/menu/menuSpritesInit.asm
+++ actors/menu/menuSpritesInit.asm
@@ -12,48 +12,11 @@ menuSpritesInit:
 	sub $02
 	and $3F
 	ld de, menuSpritesInit.loader_actor
-	call spawnActorV
-	
-	swapInRam on_deck
-	ld a, $D1
-	ld [on_deck.active_buffer], a
-	
-	restoreBank "ram"
+	call spawnActorV ;load the starting tiles
 	restoreBank "ram"
 	
-	updateActorMain menuSpritesInit.doInterrupt
-	
-	ret
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-.doInterrupt:
-	
-	swapInRam up_next
-	ld hl, up_next + $50
-	ld de, $0004
-	ld a, $14
-	
-	.spriteLoop1:
-		ld [hl], d
-		add hl, de
-		dec a
-	jr nz, menuSpritesInit.spriteLoop1
-	
-	ld hl, up_next_2 + $0050
-	ld a, $14
-	.spriteLoop2:
-		ld [hl], d
-		add hl, de
-		dec a
-	jr nz, menuSpritesInit.spriteLoop2
-	
-	restoreBank "ram"
-	
-	ld hl, $FF0F
-	res 1, [hl]
-	ld l, $FF
-	set 1, [hl]
+	ld hl, $FF40
+	set 2, [hl] ;change sprites to 8x16 mode
 	
 	ld e, c
 	ld d, b
