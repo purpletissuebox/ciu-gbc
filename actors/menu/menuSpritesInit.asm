8d24d5a310a81f63c7bb571b480f1aba4c3f9646
purple tissue box <dirksenmatthew1@gmail.com>
2023-12-02 11:51:01 -0500

    Added loop to clear out buffer sprites. Actor now initializes hardware registers.

--- actors/menu/menuSpritesInit.asm
+++ actors/menu/menuSpritesInit.asm
@@ -15,8 +15,27 @@ menuSpritesInit:
 	call spawnActorV ;load the starting tiles
 	restoreBank "ram"
 	
+	swapInRam on_deck
+	ld e, $28
+	ld hl, on_deck
+	xor a
+	.loop:
+		ldi [hl], a ;y position
+		ldi [hl], a ;x position
+		inc hl
+		ldi [hl], a ;palette
+		dec e
+	jr nz, menuSpritesInit.loop
+	restoreBank "ram"
+	
 	ld hl, $FF40
 	set 2, [hl] ;change sprites to 8x16 mode
+	ld l, $45
+	ld [hl], $5A ;set LYC register
+	ld l, $0F
+	res 1, [hl] ;stop the scanline interrupt from running immediately if it was already queued
+	ld l, $FF
+	set 1, [hl] ;enable future scanline interrupts
 	
 	ld e, c
 	ld d, b
