01f7dffcd00c623f437dc5b95c24bb9bd5b5e8d6
purple tissue box <dirksenmatthew1@gmail.com>
2023-12-03 15:31:47 -0500

    Actor now initializes sprites in the 'down' position. Added second loop to clear out both OAM buffers. Initializes LYC buffer.

--- actors/menu/menuSpritesInit.asm
+++ actors/menu/menuSpritesInit.asm
@@ -9,8 +9,9 @@ menuSpritesInit:
 .loadSprites:
 	swapInRam save_file
 	ld a, [last_played_song]
-	sub $02
+	dec a
 	and $3F
+	or $80
 	ld de, menuSpritesInit.loader_actor
 	call spawnActorV ;load the starting tiles
 	restoreBank "ram"
@@ -26,17 +27,33 @@ menuSpritesInit:
 		ldi [hl], a ;palette
 		dec e
 	jr nz, menuSpritesInit.loop
-	restoreBank "ram"
 	
+	ld e, $28
+	ld hl, on_deck_2
+	xor a
+	.loop2:
+		ldi [hl], a ;y position
+		ldi [hl], a ;x position
+		inc hl
+		ldi [hl], a ;palette
+		dec e
+	jr nz, menuSpritesInit.loop2
+	
+	ld a, $D0
+	ld [on_deck.active_buffer], a
+
 	ld hl, $FF40
 	set 2, [hl] ;change sprites to 8x16 mode
 	ld l, $45
-	ld [hl], $5A ;set LYC register
+	ld a, $7A
+	ld [hl], a ;set LYC register
+	ld [on_deck.LYC_buffer], a ;and its buffer
 	ld l, $0F
 	res 1, [hl] ;stop the scanline interrupt from running immediately if it was already queued
 	ld l, $FF
 	set 1, [hl] ;enable future scanline interrupts
 	
+	restoreBank "ram"
 	ld e, c
 	ld d, b
 	jp removeActor
