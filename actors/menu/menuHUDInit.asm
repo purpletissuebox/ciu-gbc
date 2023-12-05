1dd61dbaf362f1d389789e7849992bd605cbcd9f
purple tissue box <dirksenmatthew1@gmail.com>
2023-12-05 18:59:00 -0500

    Added dummy actor to initialize the HUD on scene load.

--- actors/menu/menuHUDInit.asm
+++ actors/menu/menuHUDInit.asm
@@ -1,6 +1,13 @@
 SECTION "MENU HUD", ROMX
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;loads tiles, attributes, and map for the hud on the menu scene.
+;spawns child to load dynamic maps - the score and selected difficulty
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 menuHUDInit:
+	swapInRam last_played_song
 	ld e, c
 	ld d, b
+	restoreBank "ram"
 	jp removeActor
