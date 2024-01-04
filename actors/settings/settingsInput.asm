4e8508e7f80806dec719c17a3e9404eb3a0b3b56
purple tissue box <dirksenmatthew1@gmail.com>
2024-01-04 10:09:12 -0500

    Initial implementation.

--- actors/settings/settingsInput.asm
+++ actors/settings/settingsInput.asm
@@ -4,7 +4,109 @@ SECTION "SETTINGS INPUT", ROMX
 ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+CURRENTOPTION = $0004
+
 settingsInput:
+.init:
+	swapInRam save_file
+	ld hl, CURRENTOPTION
+	add hl, bc
+	ld a, [last_selected_option]
+	ld [hl], a
+	restoreBank "ram"
+	
+	updateActorMain settingsInput.main
+	ret
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+.main:
+	ldh a, [scene]
+	cp SETTINGS
+	ret nz
+	
+	ldh a, [press_input]
+	
+	bit 7, a
+	jr z, settingsInput.checkUp
+		jp settingsInput.down
+	.checkUp:
+	bit 6, a
+	jr z, settingsInput.checkLeft
+		jp settingsInput.up
+	.checkLeft:
+	bit 5, a
+	jr z, settingsInput.checkRight
+		jp settingsInput.left
+	.checkRight:
+	bit 4, a
+	jr z, settingsInput.checkStart
+		jp settingsInput.right
+	.checkStart:
+	bit 3, a
+	jr z, settingsInput.checkSelect
+		jp settingsInput.start
+	.checkSelect:
+	bit 2, a
+	jr z, settingsInput.checkB
+		jp settingsInput.select
+	.checkB:
+	bit 1, a
+	jr z, settingsInput.checkA
+		jp settingsInput.B
+	.checkA:
+	bit 0, a
+	ret z
+		jp settingsInput.A
 	ld e, c
 	ld d, b
 	jp removeActor
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+.left:
+.up:
+	ld hl, CURRENTOPTION
+	add hl, bc
+	ld a, [hl]
+	dec a
+	and $07
+	ldi [hl], a
+	ret
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+.right:
+.down:
+	ld hl, CURRENTOPTION
+	add hl, bc
+	ld a, [hl]
+	inc a
+	and $07
+	ldi [hl], a
+	ret
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+.start:
+.A:
+	ret
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+.B:
+.select:
+	ld a, MENU
+	ldh [scene], a
+	
+	ld de, settingsInput.wipe_actor
+	call spawnActor
+	
+	ld e, c
+	ld d, b
+	jp removeActor
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+.wipe_actor:
+	NEWACTOR settingsScroll, $80
--- actors/settings/settingsScroll.asm
+++ actors/settings/settingsScroll.asm
@@ -4,7 +4,53 @@ SECTION "SETTINGS SCROLL", ROMX
 ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+VARIABLE = $0003
+SCROLLINDEX = $0004
+
 settingsScroll:
-	ld e, c
-	ld d, b
-	jp removeActor
+.init:
+	ld hl, VARIABLE
+	add hl, bc
+	ldi a, [hl]
+	and $80
+	rrca
+	rrca
+	ld [hl], a
+	updateActorMain settingsScroll.main
+	
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+.main:
+	ld hl, SCROLLINDEX
+	add hl, bc
+	ld a, [hl]
+	inc a
+	ld [hl], a
+	and $1F
+	jr nz, settingsScroll.doScroll
+		ld e, c
+		ld d, b
+		jp removeActor
+	.doScroll:
+	
+	ld a, [hl]
+	ld de, settingsScroll.scrollTable
+	add e
+	ld e, a
+	ld a, d
+	adc $00
+	ld d, a
+	
+	swapInRam shadow_winloc
+	ld a, [de]
+	ld [shadow_winloc], a
+	restoreBank "ram"
+	ret
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+.scrollTable:
+	;up
+	db $FF, $80, $70, $64, $5A, $51, $49, $42, $3B, $35, $30, $2B, $26, $22, $1E, $1A, $17, $14, $11, $0E, $0C, $0A, $08, $07, $05, $04, $03, $02, $01, $01, $00, $00
+	;down
+	db $FF, $00, $10, $1C, $26, $2F, $37, $3E, $45, $4B, $50, $55, $5A, $5E, $62, $66, $69, $6C, $6F, $72, $74, $76, $78, $79, $7B, $7C, $7D, $7E, $7F, $7F, $80, $80
