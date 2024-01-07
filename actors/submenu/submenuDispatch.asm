7bbaa485e7379c4487db9fb5b21774b259ed50b4
purple tissue box <dirksenmatthew1@gmail.com>
2024-01-07 14:55:10 -0500

    Initial implementation.

--- actors/submenu/submenuDispatch.asm
+++ actors/submenu/submenuDispatch.asm
@@ -4,7 +4,35 @@ SECTION "SUBMENU DISPATCH", ROMX
 ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+VARIABLE = $0003
+
 submenuDispatch:
+	ld hl, VARIABLE
+	add hl, bc
+	ld a, [hl]
+	add a
+	
+	ld de, submenuDispatch.actor_table
+	add e
+	ld e, a
+	ld a, d
+	adc $00
+	ld d, a
+	call spawnActor
+	
 	ld e, c
 	ld d, b
 	jp removeActor
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+.actor_table:
+	NEWACTOR submenuSpeed, $FF
+	NEWACTOR submenuSkin, $FF
+	NEWACTOR submenuRebind, $FF
+	NEWACTOR submenuSort, $FF
+	NEWACTOR submenuDelay, $FF
+	NEWACTOR submenuLeadin, $FF
+	NEWACTOR submenuBackground, $FF
+	NEWACTOR submenuColors, $FF
+	NEWACTOR submenuJudgement, $FF
