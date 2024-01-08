3342c169b4c0006a6ba8abd24a44db5c86490d98
purple tissue box <dirksenmatthew1@gmail.com>
2024-01-07 19:52:13 -0500

    Added comments.

--- actors/submenu/submenuDispatch.asm
+++ actors/submenu/submenuDispatch.asm
@@ -1,7 +1,7 @@
 SECTION "SUBMENU DISPATCH", ROMX
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;
+;chooses a handler according to its variable to load a submenu.
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 VARIABLE = $0003
@@ -9,21 +9,21 @@ VARIABLE = $0003
 submenuDispatch:
 	ld hl, VARIABLE
 	add hl, bc
-	ld a, [hl]
+	ld a, [hl] ;get which submenu we are on
 	add a
-	add a
+	add a ;a = submenu*sizeof(actor)
 	
 	ld de, submenuDispatch.actor_table
 	add e
 	ld e, a
 	ld a, d
 	adc $00
-	ld d, a
-	call spawnActor
+	ld d, a ;de points to the handler of choice
+	call spawnActor ;spawn it
 	
 	ld e, c
 	ld d, b
-	jp removeActor
+	jp removeActor ;done!
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
--- actors/submenu/submenuLeadin.asm
+++ actors/submenu/submenuLeadin.asm
@@ -1,7 +1,9 @@
 SECTION "SUBMENU LEADIN", ROMX
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;
+;handler for the "lead-in time" submenu.
+;displays a menu with 4 digits that be changed
+;reads and writes to the lead-in time in the save file.
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 CURRENTDIGIT = $0010
@@ -13,20 +15,20 @@ SETCHARMAP settingsChars
 submenuLeadin:
 .init:
 	ld de, submenuLeadin.task
-	call loadGraphicsTask
+	call loadGraphicsTask ;the region of the map for the submenu will stay constant, so pre-load a task for it
 	
 	swapInRam save_file
 	
 	ld hl, leadin_time
 	ldi a, [hl]
 	ld h, [hl]
-	ld l, a
+	ld l, a ;get hl = lead-in time from the save file
+	call submenuLeadin.itoa14 ;de = 4 digits in bcd
 	
-	call submenuLeadin.itoa14
 	ld hl, CURRENTDIGIT
 	add hl, bc
 	xor a
-	ldi [hl], a
+	ldi [hl], a ;start with the first digit selected, now hl points to the digit array
 	
 	ld a, e
 	and $0F
@@ -41,7 +43,7 @@ submenuLeadin:
 	ld a, d
 	swap a
 	and $0F
-	ldi [hl], a
+	ldi [hl], a ;write bcd number to the digit array
 	
 	swapInRam shadow_wmap
 	ld hl, shadow_wmap + 32*14 + 1
@@ -49,14 +51,14 @@ submenuLeadin:
 	rst $20
 	ld hl, shadow_wmap + 32*16 + 1
 	ld de, submenuLeadin.cancel_msg
-	rst $20
+	rst $20 ;write static messages to the left side of the menu
 	
 	ld hl, shadow_wmap + 32*15 + 17
 	ld a, "m"
 	ldi [hl], a
-	ld [hl], "s"
+	ld [hl], "s" ;write static milliseconds tag to the right side
 	
-	call submenuLeadin.renderDigits
+	call submenuLeadin.renderDigits ;write digits and arrows to window
 	restoreBank "ram"
 	restoreBank "ram"	
 	updateActorMain submenuLeadin.main
@@ -99,41 +101,41 @@ submenuLeadin:
 .up:
 	ld hl, CURRENTDIGIT
 	add hl, bc
-	ldi a, [hl]
+	ldi a, [hl] ;a = digit to increment, hl points to digit array already
 	add l
 	ld l, a
 	ld a, h
 	adc $00
-	ld h, a
+	ld h, a ;hl points to digit to increment
 	
 	ld a, [hl]
-	inc a
+	inc a ;increment it
 	cp $0A
-	ld [hl], a
+	ld [hl], a ;if it didnt overflow, write it as-is
 	jr nz, submenuLeadin.wrapUp
-		ld [hl], $00
+		ld [hl], $00 ;otherwise wrap to 0
 	.wrapUp:
-	jp submenuLeadin.renderDigits
+	jp submenuLeadin.renderDigits ;render the new numbers
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 .down:
 	ld hl, CURRENTDIGIT
 	add hl, bc
-	ldi a, [hl]
+	ldi a, [hl] ;a = digit to decrement, hl points to the digit array already
 	add l
 	ld l, a
 	ld a, h
 	adc $00
-	ld h, a
+	ld h, a ;hl points to digit to decrement
 	
 	ld a, [hl]
-	sub $01
-	ld [hl], a
+	sub $01 ;decrement it
+	ld [hl], a ;if it didnt underflow, write it as-is
 	jr nc, submenuLeadin.wrapDown
-		ld [hl], $09
+		ld [hl], $09 ;otherwise, wrap to 9
 	.wrapDown:
-	jp submenuLeadin.renderDigits
+	jp submenuLeadin.renderDigits ;render the new numbers
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -143,8 +145,8 @@ submenuLeadin:
 	ld a, [hl]
 	inc a
 	and $03
-	ld [hl], a
-	jp submenuLeadin.renderDigits
+	ld [hl], a ;arrow now appears over the digit to the left mod 4
+	jp submenuLeadin.renderDigits ;render it
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -154,7 +156,7 @@ submenuLeadin:
 	ld a, [hl]
 	dec a
 	and $03
-	ld [hl], a
+	ld [hl], a ;arrow now appears over the digit to the right mod 4
 	jp submenuLeadin.renderDigits
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -162,26 +164,27 @@ submenuLeadin:
 .A:
 .start:
 	ld hl, DIGITS
-	add hl, bc
+	add hl, bc ;hl points to array of 4 digits from least to most significant
 	ldi a, [hl]
 	ld e, a
 	ldi a, [hl]
 	swap a
 	or e
-	ld e, a
+	ld e, a ;e = 10s and 1s
 	ldi a, [hl]
 	ld d, [hl]
 	swap d
 	or d
-	ld d, a
+	ld d, a ;de = digits in bcd
 	
-	call submenuLeadin.atoi14
+	call submenuLeadin.atoi14 ;convert to binary in de
 	swapInRam save_file
 	ld hl, leadin_time
 	ld a, e
 	ldi [hl], a
-	ld [hl], d
+	ld [hl], d ;save to the save file
 	restoreBank "ram"
+	;now we have successfully saved the lead in time. we exit by falling through to the B case, since that will also exit.
 
 .B:
 	swapInRam shadow_wmap
@@ -193,19 +196,20 @@ submenuLeadin:
 	rst $20
 	ld hl, shadow_wmap + 32*16 + 1
 	ld de, submenuLeadin.blank_msg
-	rst $20
+	rst $20 ;blank out the window layer where the submenu appears
 	restoreBank "ram"
 	
-	updateActorMain submenuLeadin.exit
+	updateActorMain submenuLeadin.exit ;and exit
 	jp submenuLeadin.exit
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 .renderDigits:
 	swapInRam shadow_wmap
+	;first, display the digits
 	ld hl, DIGITS
-	add hl, bc
-	ld de, shadow_wmap + 32*15 + 15
+	add hl, bc ;hl points to digit array from least to most significant
+	ld de, shadow_wmap + 32*15 + 15 ;de points to the rightmost digit
 	
 	ldi a, [hl]
 	add "0"
@@ -221,77 +225,78 @@ submenuLeadin:
 	dec de
 	ldi a, [hl]
 	add "0"
-	ld [de], a
+	ld [de], a ;save digits from right to left (least to most significant)
 	
+	;next, blank out where the old arrow was	
 	ld hl, CURRENTDIGIT
 	add hl, bc
-	ld d, [hl]
+	ld d, [hl] ;d = the position where the arrows need to appear later
 	
-	ld a, " "	
-	ld hl, shadow_wmap + 32*16+12
+	ld a, " "
+	ld hl, shadow_wmap + 32*16+12 ;bottom row
 	ldi [hl], a
 	ldi [hl], a
 	ldi [hl], a
 	ldi [hl], a
-	ld hl, shadow_wmap + 32*14+12
+	ld hl, shadow_wmap + 32*14+12 ;top row
 	ldi [hl], a
 	ldi [hl], a
 	ldi [hl], a
-	ld [hl], a
+	ld [hl], a ;hl points to the tile above the rightmost digit
 	
 	ld a, l
 	sub d
 	ld l, a
 	ld a, h
 	sbc $00
-	ld h, a
+	ld h, a ;go backwards "d" tiles, so hl now points to the tile where the arrow should appear
 	
-	ld [hl], "v"
+	ld [hl], "v" ;save up arrow
 	ld de, $0040
-	add hl, de
-	ld [hl], "x"
+	add hl, de ;go down 2 rows
+	ld [hl], "x" ;save down arrow
 	
 	restoreBank "ram"
 	
-	call submitGraphicsTask
+	call submitGraphicsTask ;normally we would submit repeatedly until the task goes through, but the caller is probably expecting to get control back pretty soon.
 	ld hl, TASKSDONE
 	add hl, bc
-	ld a, [hl]
-	ld [hl], $00
-	dec a
-	ret z
+	ld a, [hl] ;read if it went through or not
+	ld [hl], $00 ;regardless of if it did, this actor will submit several more tasks in the future, but it checks to see if the tasks completed = 1. so keep writing a zero here to avoid messing up the check later.
+	dec a ;check if number of tasks was 1
+	ret z ;if it was, then go back to the caller immediately
 	
-	updateActorMain submenuLeadin.submit
+	updateActorMain submenuLeadin.submit ;this way, lag frames are only inserted if necessary.
 	ret
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-.itoa14:
+.itoa14: ;converts 14 bit number in hl to 4 digit bcd in de
 	xor a
 	add hl, hl
-	add hl, hl ;2
+	add hl, hl ;since the upper 2 digits are 0, go ahead and shift them out
 	
 	add hl, hl
 	rla
 	add hl, hl
 	rla
-	add hl, hl ;5
-	rla
+	add hl, hl ;the next 3 bits range from 0-7, which are the same in base 10 and 16
+	rla ;so we can just shift them into a with no issue
 	
 	ld d, $04
 	.phase2:
-		add hl, hl ;9
+		add hl, hl ;the next 4 bits range from 0-127, which fits in a + carry bit.
 		adc a
-		daa
+		daa ;so daa with one register
 		dec d
 	jr nz, submenuLeadin.phase2
 	
-	rl d
-	ld e, a
+	rl d ;put carry in d
+	ld e, a ;put least significant digits in e
 	
-	ld l, $07
+	ld l, $07 ;now that 9 bits are shifted out, l is free to hold the loop counter
 	.phase3:
-		sla h ;16
+		sla h ;the remaining 7 bits range from 0-9999, which need 2 registers (de)
 		ld a, e
 		adc a
 		daa
@@ -307,33 +312,34 @@ submenuLeadin:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 .atoi14:
-	ld hl, $0004
+	;the plan is to do double dabble in reverse. but now we have no daa to do the actual math, so it will be annoying
+	ld hl, $0004 ;h will act as a canvas to hold the bits as they shift out, l is a loop counter to do one nibble
 	.nibble1:
 		srl d
 		rr e
-		rr h
+		rr h ;shift 1 bit into h
 		
 		ld a, e
 		and $0F
-		cp $08
+		cp $08 ;check if each nibble is 8 or greater
 		jr c, submenuLeadin.good14
-			sub $03
+			sub $03 ;if so, subtract 3. this makes it 5, so we correctly carried 10/2 = 5
 		.good14:
 		xor e
 		and $0F
-		xor e
+		xor e ;merge the lower 4 bits of a (which contain the corrected value) into e
 		ld e, a
-		and $F0
+		and $F0 ;check the upper nibble for an 8 or greater
 		cp $80
 		jr c, submenuLeadin.good18
-			sub $30
+			sub $30 ;if so, subtract 3 from that nibble
 		.good18:
 		xor e
 		and $F0
-		xor e
+		xor e ;merge it in
 		ld e, a
 		
-		ld a, d
+		ld a, d ;repeat for the 3rd nibble.
 		and $0F
 		cp $08
 		jr c, submenuLeadin.good1C
@@ -342,18 +348,18 @@ submenuLeadin:
 		xor d
 		and $0F
 		xor d
-		ld d, a
+		ld d, a ;the 4th nibble only has 2 bits. so the range it can take is 0-3, which we can use as-is. so we can stop processing here
 		
 		dec l
 	jr nz, submenuLeadin.nibble1
 	
-	ld l, $04
+	ld l, $04 ;calculate another 4 bits
 	.nibble2:
 		srl d
 		rr e
-		rr h
+		rr h ;shift 1 bit into h
 		
-		ld a, e
+		ld a, e ;process as we did before. this time, the lower nibble of d is not getting any 1s shifted into it, so we only do e.
 		and $0F
 		cp $08
 		jr c, submenuLeadin.good24
@@ -376,10 +382,10 @@ submenuLeadin:
 		dec l
 	jr nz, submenuLeadin.nibble2
 	
-	ld d, $04
-	.nibble3:
+	ld d, $04 ;now that weve done 8 bits, l is needed to hold the result. luckily, d is empty and can be used as a loop counter
+	.nibble3: ;calculate another 4 bits
 		srl e
-		rr l
+		rr l ;shift one bit into l
 		
 		ld a, e
 		and $0F
@@ -390,28 +396,28 @@ submenuLeadin:
 		xor e
 		and $0F
 		xor e
-		ld e, a
+		ld e, a ;only process the lower half of e
 		
 		dec d
 	jr nz, submenuLeadin.nibble3
 	
-	ld a, e
-	or l
-	swap a
+	ld a, e ;the remaining 2 bits in e form the most significant digit
+	or l ;combine with the second most significant digit, which is being held in the most significant nibble of l
+	swap a ;correct their order so the mosy significant digit is in the most significant nibble
 	ld d, a
-	ld e, h	
+	ld e, h ;return in de
 	ret
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;	
 
 .submit:
-	call submitGraphicsTask
+	call submitGraphicsTask ;attempt to load the task
 	ld hl, NUMTASKS
 	add hl, bc
-	ld a, [hl]
-	ld [hl], $00
-	dec a
-	ret nz
+	ld a, [hl] ;we will check if it went through by checking if num_tasks = 1. however, this means the next graphics task will be considered failed if num_tasks = 2
+	ld [hl], $00 ;to fix this, write a zero here for later
+	dec a ;check if the task went through
+	ret nz ;try again if it didnt
 	
 	updateActorMain submenuLeadin.main
 	ret
@@ -423,12 +429,12 @@ submenuLeadin:
 	ld hl, NUMTASKS
 	add hl, bc
 	ld a, [hl]
-	ld [hl], $00
+	ld [hl], $00 ;this works on the exact same mechanism as .submit above.
 	dec a
 	ret nz
 	
-	ld a, SETTINGS
-	ldh [scene], a
+	ld a, SETTINGS ;but this time, instead of returning to the main function, we exit the actor entirely.
+	ldh [scene], a ;flag to the settings menu actor to start taking inputs again
 	ld e, c
 	ld d, b
 	jp removeActor
